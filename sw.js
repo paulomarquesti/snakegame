0cf9d974a25d6f32f71c5b5edc0b2e38dd09e67b
Paulo Marques <paulorsmarquesti@gmail.com>
2025-09-05 11:51:38 -0300

    update index / add sw

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'snake-game-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/site.webmanifest',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
